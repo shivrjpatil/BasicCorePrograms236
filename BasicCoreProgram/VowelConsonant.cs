e04ef9fa88a9c609ed4ab9753b820f649306af20
shivrjpatil <shivrajp972@gmail.com>
2022-12-24 14:46:43 +0530

    Added vowel or consonant program

---
+++ BasicCoreProgram/VowelConsonant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class VowelConsonant
+    {
+        public void Vowel()
+        {
+            Console.WriteLine("Enter the alphabet you want to check");
+            char ch = Convert.ToChar(Console.ReadLine().ToLower());
+            switch (ch)
+            {
+                case 'a':
+                case 'i':
+                case 'o':
+                case 'u':
+                case 'e':
+                    Console.WriteLine("Entered alphabet is a vowel");
+                    break;
+                default:
+                    Console.WriteLine("Entered alphabet is a consonant");
+                    break;
+            }
+        }
+    }
+}
