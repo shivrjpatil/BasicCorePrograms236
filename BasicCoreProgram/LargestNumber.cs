4879dc0c6328074b3d7b7459d8002256921ebf66
shivrjpatil <shivrajp972@gmail.com>
2022-12-24 14:54:00 +0530

    added the finding largest number program

---
+++ BasicCoreProgram/LargestNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class LargestNumber
+    {
+        public void Largest()
+        {
+            Console.WriteLine("Enter the first number");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int second = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the third number");
+            int third = Convert.ToInt32(Console.ReadLine());
+
+            if (first > second && first > third)
+                Console.WriteLine("{0} is the largest number among all three", first);
+            else if (second > first && second > third)
+                Console.WriteLine("{0} is the largest number among all three", second);
+            else
+                Console.WriteLine("{0} is the largest number among all three", third);
+        }
+    }
+}
--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -12,7 +12,7 @@ namespace BasicCoreProgram
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to c# Basic core Programs");
-            Console.WriteLine("1.FlipCoin \n2.LeapYear\n3.Powere of Two\n4.Harmonic Number\n5.Prime Factor\n6.Quotient and Reaminder\n7.Swap Two Numbers\n8.Check Even Odd\n9.Vowel or Consonant");
+            Console.WriteLine("1.FlipCoin \n2.LeapYear\n3.Powere of Two\n4.Harmonic Number\n5.Prime Factor\n6.Quotient and Reaminder\n7.Swap Two Numbers\n8.Check Even Odd\n9.Vowel or Consonant\n10.Largest among Three Numbers");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -51,6 +51,10 @@ namespace BasicCoreProgram
                 case 9:
                     VowelConsonant vowelConsonant=new VowelConsonant();
                     vowelConsonant.Vowel();
+                    break;
+                case 10:
+                    LargestNumber largestNumber = new LargestNumber();
+                    largestNumber.Largest();
                     break;
             }
             Console.ReadLine();
