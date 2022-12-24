e5c57cf4bc6db788fc43fd23f66e66d0bb01dd30
shivrjpatil <shivrajp972@gmail.com>
2022-12-24 10:56:32 +0530

    Added Even Odd Program

---
+++ BasicCoreProgram/EvenOdd.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class EvenOdd
+    {
+       public void Check()
+        {
+            Console.WriteLine("Enter the Number");
+            int num=Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+                Console.WriteLine("The number is even");
+            else
+                Console.WriteLine("the number is odd");
+        }
+    }
+}
--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -12,7 +12,7 @@ namespace BasicCoreProgram
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to c# Basic core Programs");
-            Console.WriteLine("1.FlipCoin \n2.LeapYear\n3.Powere of Two\n4.Harmonic Number\n5.Prime Factor\n6.Quotient and Reaminder\n7.Swap Two Numbers");
+            Console.WriteLine("1.FlipCoin \n2.LeapYear\n3.Powere of Two\n4.Harmonic Number\n5.Prime Factor\n6.Quotient and Reaminder\n7.Swap Two Numbers\n8.Check Even Odd");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -43,6 +43,10 @@ namespace BasicCoreProgram
                 case 7:
                     SwapTwoNumbers swapTwoNumbers = new SwapTwoNumbers();
                     swapTwoNumbers.Swap();
+                    break;
+                case 8:
+                    EvenOdd evenOdd = new EvenOdd();
+                    evenOdd.Check();    
                     break;
             }
             Console.ReadLine();
